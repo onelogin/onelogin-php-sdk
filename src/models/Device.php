8c43192140482bb90959359579fb4406c2d6191f
Sixto Martin <pitbulk@gmail.com>
2017-11-18 23:10:45 +0100

    Added extra two fields for Duo Security device type

--- src/models/Device.php
+++ src/models/Device.php
@@ -10,6 +10,12 @@ class Device
     /** @var string */
     protected $type;
 
+    /** @var string */
+    protected $duoSigRequest;
+
+    /** @var string */
+    protected $duoApiHostname;
+
     /**
     * Create a new instance of Device.
     */
@@ -17,6 +23,8 @@ class Device
     {
         $this->id = isset($data->device_id)? (int) $data->device_id : null;
         $this->type = $data->device_type;
+        $this->duoSigRequest = isset($data->duo_sig_request)? $data->duo_sig_request : null;
+        $this->duoApiHostname = isset($data->duo_api_hostname)? $data->duo_api_hostname : null;
     }
 
     public function getID()
@@ -27,5 +35,15 @@ class Device
     public function getType()
     {
         return $this->type;
+    }
+
+    public function getDuoSigRequest()
+    {
+        return $this->duoSigRequest;
+    }
+
+    public function getDuoApiHostname()
+    {
+        return $this->duoApiHostname;
     }
 }
